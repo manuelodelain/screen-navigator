528e4e9c9b7a12c5032f203078d1818ecbdd71d1
ManuelOdelain <hello@manuelodelain.com>
2018-08-03 21:41:08 +0200

    refactor main class to es6

--- src/ScreenNavigator.js
+++ src/ScreenNavigator.js
@@ -1,154 +1,146 @@
-var TinyEmitter = require('tiny-emitter');
-var inherits = require('inherits');
-var ScreenNavigatorItem = require('./ScreenNavigatorItem.js');
-var Transitions = require('./Transitions.js');
+import EventEmitter from 'tiny-emitter';
+import ScreenNavigatorItem from './ScreenNavigatorItem';
+import Transitions from './Transitions';
+import AScreen from './AScreen';
 
-var ScreenNavigator = function(){
-  this.items = {};
+export default class ScreenNavigator extends EventEmitter {
+  constructor () {
+    this.items = {};
 
-  this.currentItemId = null;
-  this.previousItemId = null;
+    this.currentItemId = null;
+    this.previousItemId = null;
 
-  this.currentScreen = null;
-  this.previousScreen = null;
+    this.currentScreen = null;
+    this.previousScreen = null;
 
-  this.transition = ScreenNavigator.defaultTransition;
-  this.transitionRunning = false;
-  this.transitionCancel = null;
-};
+    this.transition = ScreenNavigator.defaultTransition;
+    this.transitionRunning = false;
+    this.transitionCancel = null;
 
-inherits(ScreenNavigator, TinyEmitter);
-
-ScreenNavigator.defaultTransition = Transitions.none;
-
-ScreenNavigator.prototype.addItem = function(id, screen, options) {
-  var item = new ScreenNavigatorItem(screen, options);
-
-  this.items[id] = item;
-
-  return item;
-};
-
-ScreenNavigator.prototype.getItem = function(id) {
-  return this.items[id];
-};
-
-ScreenNavigator.prototype.showScreen = function(id, transition, options) {
-  if (!this.getItem(id)){
-    throw new Error('ScreenNavigator - the item with the id ' + id + ' doesn\'t exist');
-  }
-
-  if (this.transitionRunning){
-    this.onTransitionComplete(true);
-  } 
-
-  if (this.currentScreen){
-    this.previousItemId = this.currentItemId;
-    this.previousScreen = this.currentScreen;
-  }
-
-  this.currentItemId = id;
-
-  this.onScreenChange();
-
-  this.startTransition(transition, options);
-};
-
-ScreenNavigator.prototype.clearScreen = function(transition) {
-  if (!this.currentScreen){
-    return;
-  }
-
-  this.previousItemId = this.currentItemId;
-  this.previousScreen = this.currentScreen;
-
-  this.currentItemId = null;
-
-  this.onScreenChange();
-
-  this.startTransition(transition);
-};
-
-ScreenNavigator.prototype.startTransition = function(transition, options) {
-  transition = transition || this.transition;
-
-  var currentItem = this.getItem(this.currentItemId);
-
-  if (options) currentItem.setOptions(options);
-
-  this.currentScreen = currentItem ? currentItem.getScreen(options) : null;
-
-  this.transitionRunning = true;
-
-  this.emit('transitionStart');
-
-  this.transitionCancel = transition(this.currentScreen, this.previousScreen, this.onTransitionComplete.bind(this));
-};
-
-ScreenNavigator.prototype.onScreenChange = function() {
-  this.emit('screenChange');
-};
-
-ScreenNavigator.prototype.onTransitionComplete = function(cancelTransition, silent) {
-  this.transitionRunning = false;
-
-  if (cancelTransition){
-    if (this.transitionCancel) this.transitionCancel();
+    ScreenNavigator.defaultTransition = Transitions.none;
   }
   
-  this.disposePreviousScreen();
+  addItem (id, screen, options) {
+    const item = new ScreenNavigatorItem(screen, options);
+  
+    this.items[id] = item;
+  
+    return item;
+  }
 
-  if (!silent){
-    if (cancelTransition){
-      this.emit('transitionCancel');
-    }else{
-      this.emit('transitionComplete');
+  getItem (id) {
+    return this.items[id];
+  }
+
+  showScreen (id, transition, options) {
+    if (!this.getItem(id)){
+      throw new Error('ScreenNavigator - the item with the id ' + id + ' doesn\'t exist');
     }
+  
+    if (this.transitionRunning){
+      this.onTransitionComplete(true);
+    } 
+  
+    if (this.currentScreen){
+      this.previousItemId = this.currentItemId;
+      this.previousScreen = this.currentScreen;
+    }
+  
+    this.currentItemId = id;
+  
+    this.onScreenChange();
+  
+    this.startTransition(transition, options);
   }
 
-  this.transitionCancel = null;
-};
-
-ScreenNavigator.prototype.dispose = function(forceDispose) {
-  if (typeof forceDispose !== 'boolean') forceDispose = true;
-
-  if (this.transitionRunning){
-    this.onTransitionComplete(true, true);
+  clearScreen (transition) {
+    if (!this.currentScreen){
+      return;
+    }
+  
+    this.previousItemId = this.currentItemId;
+    this.previousScreen = this.currentScreen;
+  
+    this.currentItemId = null;
+  
+    this.onScreenChange();
+  
+    this.startTransition(transition);
   }
 
-  this.disposeCurrentScreen();
-  this.disposePreviousScreen();
-
-  var item;
-
-  for (var itemId in this.items){
-    this.items[itemId].dispose(forceDispose);
-
-    delete this.items[itemId];
+  startTransition (transition, options) {
+    transition = transition || this.transition;
+  
+    const currentItem = this.getItem(this.currentItemId);
+  
+    if (options) currentItem.setOptions(options);
+  
+    this.currentScreen = currentItem ? currentItem.getScreen(options) : null;
+  
+    this.transitionRunning = true;
+  
+    this.emit('transitionStart');
+  
+    this.transitionCancel = transition(this.currentScreen, this.previousScreen, this.onTransitionComplete.bind(this));
   }
 
-  this.transition = null;
-};
+  onScreenChange () {
+    this.emit('screenChange');
+  }
 
-ScreenNavigator.prototype.disposePreviousScreen = function() {
-  if (!this.previousScreen) return;
+  onTransitionComplete (cancelTransition, silent) {
+    this.transitionRunning = false;
+  
+    if (cancelTransition){
+      if (this.transitionCancel) this.transitionCancel();
+    }
+    
+    this.disposePreviousScreen();
+  
+    if (!silent){
+      if (cancelTransition){
+        this.emit('transitionCancel');
+      }else{
+        this.emit('transitionComplete');
+      }
+    }
+  
+    this.transitionCancel = null;
+  }
 
-  this.getItem(this.previousItemId).disposeScreen(this.previousScreen);
+  dispose (forceDispose) {
+    if (typeof forceDispose !== 'boolean') forceDispose = true;
+  
+    if (this.transitionRunning){
+      this.onTransitionComplete(true, true);
+    }
+  
+    this.disposeCurrentScreen();
+    this.disposePreviousScreen();
+  
+    for (let itemId in this.items){
+      this.items[itemId].dispose(forceDispose);
+  
+      delete this.items[itemId];
+    }
+  
+    this.transition = null;
+  }
 
-  this.previousScreen = null;
-};
+  disposePreviousScreen () {
+    if (!this.previousScreen) return;
+  
+    this.getItem(this.previousItemId).disposeScreen(this.previousScreen);
+  
+    this.previousScreen = null;
+  }
 
-ScreenNavigator.prototype.disposeCurrentScreen = function() {
-  if (!this.currentScreen) return;
-
-  this.getItem(this.currentItemId).disposeScreen(this.currentScreen);
-
-  this.currentScreen = null;
-};
-
-module.exports = ScreenNavigator;
-
-module.exports.AScreen = require('./AScreen.js');
-module.exports.ScreenNavigatorItem = ScreenNavigatorItem;
-module.exports.Transitions = Transitions;
+  disposeCurrentScreen () {
+    if (!this.currentScreen) return;
+  
+    this.getItem(this.currentItemId).disposeScreen(this.currentScreen);
+  
+    this.currentScreen = null;
+  }
+}
 
